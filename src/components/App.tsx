48d66443756573435cd2249e2aacd1611820f037
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2025-04-19 17:51:54 +0900

    死んじゃった

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Box, CssBaseline, FormControlLabel, Switch, Typography } from '@mui/material';
+import { storage } from '../utils/localStorage';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 type Props = {
   title: string;
   toggleDelete: (isDelete: boolean) => void;
 };
 
-export const App: React.FC<Props> = ({ title }) => {
-  return <div style={{ color: 'red' }}>{title}</div>;
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
+
+export const App: React.FC<Props> = ({ title, toggleDelete }) => {
+  const onChange = (e: unknown, checked: boolean) => toggleDelete(checked);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography
+          sx={{
+            fontSize: 'var(--font-size-xxl)',
+            fontWeight: 'var(--font-weight-xxl)',
+            lineHeight: 'var(--font-line-height-xxl)',
+          }}
+        >
+          {title}
+        </Typography>
+        <FormControlLabel
+          control={<Switch onChange={onChange} defaultChecked={storage.isDelete} />}
+          label="殺しますよ"
+          labelPlacement="start"
+        />
+      </Box>
+    </ThemeProvider>
+  );
 };
